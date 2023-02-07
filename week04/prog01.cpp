640b8414aa103be406d09bc964327c6235b9af7d
Oliver baltzer <114311552+ObaltzerS@users.noreply.github.com>
2023-02-06 21:12:08 -0800

    rudimentary bubble sort program

---
+++ week04/prog01.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int sortdata[15] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    int n = sizeof(sortdata) / sizeof(int);
+    int temp, i, j, comps = 0, swaps = 0;
+    cout << "size of list is " << n << " elements" << "\n";
+    for (i = 0; i < n-1; i++){
+        //repeat comparisons
+        for (j = 0; j < n-i-1; j++){
+            //iterate through list, minus the elements which are sorted
+            comps++;
+            if(sortdata[j] > sortdata[j+1]){
+                swaps++;
+                //swap elements of list if the next element is larger
+                temp = sortdata[j+1]; 
+                sortdata[j+1] =  sortdata[j];
+                sortdata[j] = temp;
+            }
+        }
+        for (int k = 0; k < n; k++){
+        //output the list after each comparison is finished. 
+        cout << sortdata[k] << ",";
+    }
+        cout << "\n";
+    }
+    cout << comps << " total comparisons performed" << "\n";
+    cout << swaps << " total element swaps performed" << "\n";
+    cout << "final sorted list: ";
+    for (int k = 0; k < n; k++){
+        //output list
+        cout << sortdata[k] << ",";
+    }
+    return 0;
+}
