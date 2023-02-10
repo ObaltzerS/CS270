50806d1db423a6b3d4262fe5bf7b597c4f802a35
Oliver baltzer <114311552+ObaltzerS@users.noreply.github.com>
2023-02-10 10:47:22 -0800

    modules practice

--- week04/modules/realdeal/main.cpp
+++ week04/modules/realdeal/main.cpp
@@ -1,8 +1,8 @@
 //main file
 #include "module3.h"
 #include <iostream>
-
 using namespace std;
+
 int main(){
     cout << sum(5,2); // calling the sum function from module3
     return 0;
--- week04/modules/realdeal/module3.cpp
+++ week04/modules/realdeal/module3.cpp
@@ -1,7 +1,6 @@
 // implimentation
 // a function which sums two integers, returns the sum
 // this is the implimentation
-
 int sum(int num1, int num2){ // interface, what tells the user how to use the function
     
     return num1 + num2;
